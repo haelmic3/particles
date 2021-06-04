bc8439c2e269c573604193f3a29dfa493ea63411
Michael (mx) <haelmic3@gmail.com>
2021-06-04 15:48:14 -0400

    added max min average fps metric overall and 10sec

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@
 #include <thread>
 #include <vector>
 #include <random>
+#include <limits>
+#include <type_traits>
 #include <SFML/Graphics.hpp>
 
 constexpr char TITLE[] = "Hackz for days"; // TITLE
@@ -25,6 +27,20 @@ struct Model_delta
 // TODO
 	std::chrono::nanoseconds time;
 	
+};
+template <typename T>struct Stat
+{
+	T min = std::numeric_limits<T>::max();
+	T max = 0;
+	T ave = 0;
+	unsigned long long count = 0;
+	void add(T n)
+	{
+		if(n < min)min = n;
+		if(max < n)max = n;
+		ave = (static_cast<T>(count) * ave + n) / static_cast<T>(1+count);
+		count += 1;
+	}
 };
 template<typename T,unsigned long long Size>
 struct Array
@@ -86,17 +102,31 @@ class View : public sf::Drawable
 	using clock = std::chrono::steady_clock;
 	sf::RectangleShape tl;
 	sf::Font freefont;
-	sf::Text stats[2];
+	sf::Text stats[8];
 	Graph graph;
 	unsigned width,height;
+	Stat<double> fps;
+	Stat<double> full_fps;
  public:
-	View():tl({5,5}),freefont(),stats(),graph()
+	View():tl({5,5}),freefont(),stats(),graph(),width{},height{},fps{}
 	{
 		tl.setFillColor(sf::Color::Red);
 		freefont.loadFromFile("FreeMono.ttf");
 		stats[0] = sf::Text(sf::String("Test"),freefont);
 		stats[1] = sf::Text(sf::String("Test"),freefont);
-		stats[1].setPosition({220,0});
+		stats[1].setPosition({230,0});
+		stats[2] = sf::Text(sf::String("Test"),freefont);
+		stats[2].setPosition({530,0});
+		stats[3] = sf::Text(sf::String("Test"),freefont);
+		stats[3].setPosition({760,0});
+		stats[4] = sf::Text(sf::String("Test"),freefont);
+		stats[4].setPosition({990,0});
+		stats[5] = sf::Text(sf::String("Test"),freefont);
+		stats[5].setPosition({1200,0});
+		stats[6] = sf::Text(sf::String("Test"),freefont);
+		stats[6].setPosition({1450,0});
+		stats[7] = sf::Text(sf::String("Test"),freefont);
+		stats[7].setPosition({1700,0});
 		for(int i = -1000; i <= 1000; i++)
 		{
 			float k = static_cast<float>(i)/5.f;
@@ -112,6 +142,33 @@ class View : public sf::Drawable
 		graph.states.transform.translate(hw,hh).scale(1.f,-1.f);
 		graph.clip(-hw,hw,-hh,hh);
 	}
+	template<typename T> inline const T bXdigits(T num)
+	{
+		using namespace std;
+		static_assert(is_integral<decltype(num)>::value,"Assumes integral type.");
+		return num < 1000000000?
+		(
+			num < 10000
+			?	num < 100
+				?	num < 10
+					?	1
+					:	2
+				:	num < 1000
+					?	3
+					:	4
+			:	num < 1000000
+				?	num < 100000
+					?	5
+					:	6
+				:	num < 10000000
+					?	7
+					:	num < 100000000
+						?	8
+						:	9
+		):(
+			throw "Couldn't be bothered to support 10+digits"
+		);
+	}
 	void update(Model_delta model)
 	{
 		auto t = model.time.count();
@@ -122,26 +179,30 @@ class View : public sf::Drawable
 		auto out0
 			= std::to_string(seconds)
 			+ "."
-			+ &nano
-				[
-					partial < 10? 1:
-					partial < 100? 2:
-					partial < 1000? 3:
-					partial < 10000? 4:
-					partial < 100000? 5:
-					partial < 1000000? 6:
-					partial < 10000000? 7:
-					partial < 100000000? 8:
-					9
-				]
+			+ &nano[bXdigits(partial)]
 			+ std::to_string(partial)
 		;
+		auto cur = 1000000000.0/t;
 		auto out1
-			= std::to_string(1000000000.0/t)
+			= std::to_string(cur)
 			+ "fps "
 		;
+		fps.add(cur);
+		full_fps.add(cur);
 		stats[0].setString(out0);
 		stats[1].setString(out1);
+		stats[2].setString(std::to_string(full_fps.min));
+		stats[3].setString(std::to_string(fps.min));
+		stats[4].setString(std::to_string(fps.ave));
+		stats[5].setString(std::to_string(full_fps.ave));
+		stats[6].setString(std::to_string(fps.max));
+		stats[7].setString(std::to_string(full_fps.max));
+		if(fps.count > fps.ave*10)
+		{
+			fps.max = (fps.ave + fps.max)/2;
+			fps.min = (fps.ave + fps.min)/2;
+			fps.count /= 10;
+		}
 	}
 	virtual void draw(sf::RenderTarget& target,sf::RenderStates states)const
 	{
